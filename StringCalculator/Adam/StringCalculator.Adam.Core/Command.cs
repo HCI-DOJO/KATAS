9f3654e5e7e0aa187666f9db764c1afbc402ebb2
Brooke-Estes <brooke@estes-family.net>
2013-12-20 12:39:43 -0600

    Merge

---
+++ StringCalculator/Adam/StringCalculator.Adam.Core/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Adam.Core
+{
+    public abstract class Command
+    {
+        public abstract void Execute();
+    }
+
+    public abstract class Command<TResult> : Command
+    {
+        public TResult Result { get; protected set; }
+    }
+
+}
---
+++ StringCalculator/Adam/StringCalculator.Adam.Core/SumNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Adam.Core
+{
+    public class SumNumbers:Command<double>
+    {
+        private string _numbersToSum;
+        private string _delimiter;
+
+        public SumNumbers(string numbersToSum,string delimiter = null)
+        {
+            _numbersToSum = numbersToSum;
+            _delimiter = delimiter;
+        }
+
+        public override void Execute()
+        {
+            if (_numbersToSum == string.Empty)
+            {
+                return;
+            }
+
+            var delimiter = _delimiter;
+
+            if (_delimiter == null)
+            {
+                delimiter = new Regex(@"\D").Match(_numbersToSum.Replace("-","")).Value;
+            }
+           
+            var numbers = _numbersToSum.Split(new []{delimiter},StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string,string> matches = (d) => { return new Regex(@"-\d").Match(d).Value; };
+            
+            numbers.ToList().ForEach((n) => {
+                                                if (matches(n) != string.Empty)
+                                                {
+                                                    var d = Convert.ToDouble(n);
+                                                    throw new NegativeNumberNotAllowedException(d);
+                                                }
+            });
+
+            Result = numbers.Sum((s) => Convert.ToDouble(s));
+        }
+    }
+}
